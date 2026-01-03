486f77345c30854ee47a90faca0109ec5c2fac38
CitySc0per <seankane97@gmail.com>
2026-01-02 19:40:46 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CityGuessGame from "./CityGuessGame";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CityGuessGame />
-  </React.StrictMode>
-);
